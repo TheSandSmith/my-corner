152900e29a412b196038366ceddaa276a91f6425
Joe Bashour <hello@joebashour.dev>
2025-09-10 16:15:26 -0400

    Housekeeping
    - clean up boilerplate + add new base components
    - update configs
    - update global theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,15 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
+/* -------------------------------------------------------------------------- */
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.joebashour.dev',
-  integrations: [svelte(), mdx(), sitemap()],
+  integrations: [svelte(), mdx(), sitemap(), icon()],
 
   vite: {
     plugins: [tailwindcss()],
